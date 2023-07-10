eac45cf6b446aa7038cc8ce65de644bf0113357d
HeatherBluesky <heatherbluesky@outlook.com>
2023-07-10 11:23:19 +0100

    search

--- search.js
+++ search.js
@@ -2,6 +2,22 @@ const fs = require('fs');
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
 
+const params = {
+  headers: {
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+  },
+};
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter the search term: ', (searchTerm) => {
+  rl.close();
+
 axios.get('https://www.jjfoodservice.com/search?b&page=0&q=nestle&size=12',)
   .then(response => {
     const dom = new JSDOM(response.data);
@@ -36,14 +52,15 @@ axios.get('https://www.jjfoodservice.com/search?b&page=0&q=nestle&size=12',)
     });
 
     // Write data to JSON file
-    fs.writeFile('nestleProducts.json', JSON.stringify(data, null, 2), err => {
+    fs.writeFile('searchResults.json', JSON.stringify(data, null, 2), err => {
       if (err) {
         console.error('Error writing JSON file:', err);
       } else {
-        console.log('Data written to nestleProducts.json file');
+        console.log('Data written to searchResults.json file');
       }
     });
   })
   .catch(error => {
     console.error('Error retrieving data:', error);
   });
+});
